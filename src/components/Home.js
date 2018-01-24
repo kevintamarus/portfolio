0e3d86e5787cee4b412471ed181a5cc3b3e024de
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-01-24 02:35:01 -0800

    navbar updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ import React, {Component} from 'react';
 export default class Home extends Component {
   render() {
     return (
-      <div style={{textAlign: 'center', justifyContent:'center'}}>
-        Home Page
+      <div className="center-align">
+        <h1>Home Page</h1>
       </div>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,20 @@ export default class Navbar extends Component {
   render() {
     const {history} = this.props;
     return (
-      <div>
+      <div className="navbar-fixed">
         <nav style={{background: 'linear-gradient(to right, #3399ff 0%, #339933 100%)'}}>
           <div className="nav-wrapper">
-            <a className="brand-logo right">Home</a>
+            <a href="#!" className="brand-logo right" style={{cursor:'pointer'}} onClick={() => history.push('/')}>Home</a>
+            <a href="#" data-activates="mobile-demo" className="button-collapse" onClick={() => console.log('hamburger')}><i className="material-icons">menu</i></a>
             <ul id="nav-mobile" className="left hide-on-med-and-down">
-              <li><a onClick={() => history.push('/')}>Home</a></li>
               <li><a onClick={() => history.push('/projects')}>Projects</a></li>
-              <li><a onClick={() => history.push('/skills')}>Skills</a></li>
+              <li><a onClick={() => history.push('/skills')}>Technical Skills</a></li>
+              <li><a onClick={() => history.push('/volunteer')}>Volunteer Work</a></li>
+              <li><a onClick={() => history.push('/music')}>Music</a></li>
+            </ul>
+            <ul id="mobile-demo" className="side-nav">
+              <li><a onClick={() => history.push('/projects')}>Projects</a></li>
+              <li><a onClick={() => history.push('/skills')}>Technical Skills</a></li>
               <li><a onClick={() => history.push('/volunteer')}>Volunteer Work</a></li>
               <li><a onClick={() => history.push('/music')}>Music</a></li>
             </ul>

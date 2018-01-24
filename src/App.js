de02b828dab35e7a240743bf96cba8c5674baa00
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-01-24 01:41:35 -0800

    navbar routing is done

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
+        <Navbar history={history}/>
         <Router history={history}>
           <Switch>
             <Route exact path='/' component={Home}/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Navbar extends Component {
   render() {
+    const {history} = this.props;
     return (
       <div>
         <nav style={{background: 'linear-gradient(to right, #3399ff 0%, #339933 100%)'}}>
           <div className="nav-wrapper">
-            <a href="#" className="brand-logo right">Logo</a>
+            <a className="brand-logo right">Home</a>
             <ul id="nav-mobile" className="left hide-on-med-and-down">
-              <li><a>Projects</a></li>
-              <li><a>Volunteer Work</a></li>
-              <li><a>Music</a></li>
+              <li><a onClick={() => history.push('/')}>Home</a></li>
+              <li><a onClick={() => history.push('/projects')}>Projects</a></li>
+              <li><a onClick={() => history.push('/skills')}>Skills</a></li>
+              <li><a onClick={() => history.push('/volunteer')}>Volunteer Work</a></li>
+              <li><a onClick={() => history.push('/music')}>Music</a></li>
             </ul>
           </div>
         </nav>

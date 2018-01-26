6613cd6d19dc98cac3777df33715590b04015c41
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-01-25 23:38:26 -0800

    trying to implement modal, jquery errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {Modal} from 'react-materialize';
+import $ from 'jquery';
 
 export default class Navbar extends Component {
+
   render() {
+    console.log(this.refs, 'refs')
     const {history} = this.props;
     return (
       <div className="navbar-fixed">
         <nav style={{background: 'linear-gradient(to right, #3399ff 0%, #339933 100%)'}}>
           <div className="nav-wrapper">
             <a href="#!" className="brand-logo right" style={{cursor:'pointer'}} onClick={() => history.push('/')}>Home</a>
-            <a href="#" data-activates="mobile-demo" className="button-collapse" onClick={() => console.log('hamburger')}><i className="material-icons">menu</i></a>
+            <a href="#" 
+              data-activates="mobile-demo" 
+              className="button-collapse" 
+              onClick={() => $('#foo').modal('open')}
+            ><i className="material-icons">menu</i></a>
             <ul id="nav-mobile" className="left hide-on-med-and-down">
               <li><a onClick={() => history.push('/projects')}>Projects</a></li>
               <li><a onClick={() => history.push('/skills')}>Technical Skills</a></li>
@@ -26,6 +34,11 @@ export default class Navbar extends Component {
             </ul>
           </div>
         </nav>
+        <Modal
+          refs='foo'
+          header='Modal Header'>
+          Lorem ipsum dolor sit amet
+        </Modal>
       </div>
     );
   }

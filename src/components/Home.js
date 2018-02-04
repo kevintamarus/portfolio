6f0aa542aaeed9d5155b85a3b903138e8231d98b
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-02-03 18:01:16 -0800

    playing with D3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import BarChart from './BarChart';
+import WorldMap from './WorldMap';
 
 export default class Home extends Component {
   render() {
     return (
       <div className="center-align">
         <h1>Home Page</h1>
+        <BarChart data={[5,10,1,3]} size={[500,500]} />
+        <div>
+          <WorldMap/>
+        </div>
       </div>
     );
   }

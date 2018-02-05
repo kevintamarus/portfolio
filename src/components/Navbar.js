0c35877944785658167c81c78bbb05efdda4f45e
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-02-05 10:30:26 -0800

    working on nav color update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,47 @@ import {Modal} from 'react-materialize';
 import $ from 'jquery';
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navColorProjects: 'white',
+      navColorSkills: 'white',
+      navColorVolunteer: 'white',
+      navColorMusic: 'white',
+      navColorRunning: 'white',
+      update: false
+    }
+  }
+
+  componentDidMount() {
+    const path = this.props.history.location.pathname; 
+    if (path !== '/') {
+      const state = 'navColor' + path.charAt(1).toUpperCase() + path.slice(2);
+      this.setState({[state]: 'yellow'});
+    }
+  }
+
+  shouldComponentUpdate() {
+    return this.state.update;
+  }
+
+  changeNavColor = (color) => {
+    const state = 'navColor' + color;
+    this.setState({
+      navColorProjects: 'white',
+      navColorSkills: 'white',
+      navColorVolunteer: 'white',
+      navColorMusic: 'white',
+      navColorRunning: 'white'
+    });
+    this.setState({[state]: 'yellow', update: true});
+    console.log('hitting');
+  }
 
   render() {
     const {history} = this.props;
+    const {navColorProjects, navColorSkills, navColorVolunteer, navColorMusic, navColorRunning} = this.state;
+    console.log(history, 'history')
     return (
       <div className="navbar-fixed">
         <nav style={{background: 'linear-gradient(to right, #3399ff 0%, #339933 100%)'}}>
@@ -24,7 +62,10 @@ export default class Navbar extends Component {
               }>
               <div className="center-align">
                 <div className="modal-action modal-close" style={{cursor:'pointer'}}><a onClick={() => history.push('/')}>Home</a></div>
-                <div className="modal-action modal-close" style={{cursor:'pointer'}}><a onClick={() => history.push('/projects')}>Projects</a></div>
+                <div className="modal-action modal-close" style={{cursor:'pointer'}}><a onClick={() => {
+                  history.push('/projects');
+                  this.changeNavColor('Projects');
+                  }}>Projects</a></div>
                 <div className="modal-action modal-close" style={{cursor:'pointer'}}><a onClick={() => history.push('/skills')}>Technical Skills</a></div>
                 <div className="modal-action modal-close" style={{cursor:'pointer'}}><a onClick={() => history.push('/volunteer')}>Volunteer Work</a></div>
                 <div className="modal-action modal-close" style={{cursor:'pointer'}}><a onClick={() => history.push('/music')}>Music</a></div>
@@ -32,11 +73,11 @@ export default class Navbar extends Component {
               </div>
             </Modal>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a onClick={() => history.push('/projects')}>Projects</a></li>
-              <li><a onClick={() => history.push('/skills')}>Technical Skills</a></li>
-              <li><a onClick={() => history.push('/volunteer')}>Volunteer Work</a></li>
-              <li><a onClick={() => history.push('/music')}>Music</a></li>
-              <li><a onClick={() => history.push('/running')}>Running</a></li>
+              <li><a style={{color: navColorProjects}} onClick={() => history.push('/projects')}>Projects</a></li>
+              <li><a style={{color: navColorSkills}} onClick={() => history.push('/skills')}>Technical Skills</a></li>
+              <li><a style={{color: navColorVolunteer}} onClick={() => history.push('/volunteer')}>Volunteer Work</a></li>
+              <li><a style={{color: navColorMusic}} onClick={() => history.push('/music')}>Music</a></li>
+              <li><a style={{color: navColorRunning}} onClick={() => history.push('/running')}>Running</a></li>
             </ul>
           </div>
         </nav>
